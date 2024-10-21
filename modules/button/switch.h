3df9a607f2c6fe0a4d77c06d2dd761f9095ec58b
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-10-21 22:53:47 +0200

    Added .cpp and .h files for the button module, already implemented from previous exams

---
+++ modules/button/switch.h
@@ -0,0 +1,26 @@
+#ifndef SWITCH_H
+#define SWITCH_H
+/**
+* @file switch.h
+*
+* @brief Modulo del boton.
+*
+* @author Diego Aceituno Seoane
+*
+* Controla el boton de usuario y registra las interrupciones necesarias.
+* El boton ya tiene controlado los rebotes por diseño HardWare.
+*
+*/
+
+extern volatile int button_pressed_msg;
+extern volatile int button_released_msg;
+
+
+/**
+*
+* Registro de la interrupción y mensajes relacionados con el boton
+*
+*/
+void button_init();
+
+#endif
