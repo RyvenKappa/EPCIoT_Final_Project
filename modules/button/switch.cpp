4b95251906e6db40e1bdbd9cde440ad501c7e66e
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-10-22 11:08:24 +0200

    Removed one ISR and message of the button released, as is not needed by the requirements

--- modules/button/switch.cpp
+++ modules/button/switch.cpp
@@ -7,19 +7,13 @@
 static InterruptIn sw(USER_SWITCH);
 
 volatile int button_pressed_msg = 0;
-volatile int button_released_msg = 0;
 
 
 void switch_pressed_handler(){
     button_pressed_msg = 1;
 }
 
-void switch_released_handler(){
-    button_released_msg = 1;
-}
-
 void button_init(){
     sw.mode(PullNone);
     sw.fall(&switch_pressed_handler);
-    sw.rise(&switch_released_handler);
 }
