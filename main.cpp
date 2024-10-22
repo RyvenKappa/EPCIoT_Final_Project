6e9341fb03a4e541077133fc4a0e60a21dd2a09d
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-10-22 11:43:18 +0200

    Added the implementation for the state cycle and the board leds

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "mbed.h"
+#include "modules/button/switch.h"
+#include "modules/state_machine/state_machine.h"
 
 // main() runs in its own thread in the OS
 int main()
 {
+    //Init leds
+    button_init();
+    state_machine_init();
     while (true) {
-
+        state_machine_cycle();
     }
 }
 
--- modules/state_machine/state_machine.cpp
+++ modules/state_machine/state_machine.cpp
@@ -1,2 +1,46 @@
 #include "mbed.h"
 #include "state_machine.h"
+#include "../button/switch.h"
+
+enum STATES{
+    TEST,
+    NORMAL,
+    ADVANCED
+};
+
+static STATES actual_state;
+static BusOut board_leds(LED1,LED2,LED3);
+
+void state_machine_init(){
+    actual_state = TEST;
+    board_leds.write(1);
+}
+
+void state_machine_cycle(){
+    switch (actual_state) {
+        case TEST:
+            if(button_pressed_msg){
+                button_pressed_msg = 0;
+                actual_state = NORMAL;
+                board_leds.write(2);
+                printf("Test a Normal\n");
+            }
+        break;
+        case NORMAL:
+            if (button_pressed_msg) {
+                button_pressed_msg = 0;
+                actual_state = ADVANCED;
+                board_leds.write(4);
+                printf("Normal a Advanced\n");
+            }
+        break;
+        case ADVANCED:
+            if (button_pressed_msg) {
+                button_pressed_msg = 0;
+                actual_state = TEST;
+                board_leds.write(1);
+                printf("Advanced a Test\n");
+            }
+        break;
+    }
+}
