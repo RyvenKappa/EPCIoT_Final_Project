f6779498517eeca12ca94ebf2380a6290c6f23ca
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-10-22 10:40:38 +0200

    Added definition of the state machine module in the .h file

--- modules/state_machine/state_machine.h
+++ modules/state_machine/state_machine.h
@@ -1,5 +1,20 @@
 #ifndef STATE_MACHINE_H
 #define STATE_MACHINE_H
 
+/**
+* @file state_machine.h
+*
+* @brief State_Machine main module.
+*
+* @author Diego Aceituno Seoane
+*
+* Module that implements an state_machine to control the events of the system.
+* Controls the funcionality changes between states.
+*
+*/
 
+/**
+* Function to do one cycle of the state_machine.
+*/
+extern void state_machine_cycle();
 #endif
