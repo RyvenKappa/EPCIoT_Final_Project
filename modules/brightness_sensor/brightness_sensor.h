017278cca8842bd5f075f3455bb541d8c4f879a2
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-10-26 22:27:39 +0200

    Added definition for the brightness_sensor module and brightness_thread

--- modules/brightness_sensor/brightness_sensor.h
+++ modules/brightness_sensor/brightness_sensor.h
@@ -1,4 +1,26 @@
 #ifndef BRIGHTNESS_SENSOR_H
 #define BRIGHTNESS_SENSOR_H
+#include "mbed.h"
+/**
+* @file brightness_sensor.h
+*
+* @brief Modulo para controlar el sensor de brillo.
+*
+* @author Diego Aceituno Seoane
+*
+* Controls the inicialization of the photo-transistor for the brightness.
+* Controls of the measurements with a separate thread.
+* This module is implemented taking into account that the voltage in the collector is 3.3 Volts.
+* 
+*/
 
+/**
+* @brief Measurement thread for the brightness sensor.
+*/
+extern Thread brightness_thread;
+
+/**
+* @brief Inicialization of the brightness_sensor_thread.
+*/
+extern void brightness_sensor_init();
 #endif
