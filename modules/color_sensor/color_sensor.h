814ab1c142b4ac441f2279c9b9bb12967112c9af
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-10-28 15:52:31 +0100

    Definition of the color sensor module .h file
    Definition of the module that will manage the color sensor:
    - Independent thread.
    - color_sensor_init() function to start the thread.
    - To be controlled with signals.

--- modules/color_sensor/color_sensor.h
+++ modules/color_sensor/color_sensor.h
@@ -1,4 +1,27 @@
 #ifndef COLOR_SENSOR_H
 #define COLOR_SENSOR_H
+#include "mbed.h"
+/**
+* @file color_sensor.h
+*
+* @brief Color sensor control module.
+*
+* @author Diego Aceituno Seoane
+*
+* Controls a TCS3472 color sensor:
+* - RGB and Clear Light sensing with IR Filter.
+* - Low power states.
+*
+*/
+
+/**
+* @brief Measurement thread for the color sensor.
+*/
+extern Thread color_thread;
+
+/**
+* @brief Inicialization of the color thread.
+*/
+extern void  color_sensor_init();
 
 #endif
