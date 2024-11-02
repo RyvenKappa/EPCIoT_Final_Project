cbee0d968d877513412408fdc86cc634f7c5b10d
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-02 20:08:20 +0100

    Implementation of the temp and hum sensor .cpp
    -Independent thread with name and 256B of stack.
    -I2C communications based on the commands.
    -Relative Humidity obtained and converted into the msg.
    -Temperature from the RH measurement obtained and converted into the
    msg.
    -Msg sent into the ctrl_in_queue.
    -Signal to wait for a measurement order.

--- modules/temp_hum_sensor/temp_hum.cpp
+++ modules/temp_hum_sensor/temp_hum.cpp
@@ -2,10 +2,35 @@
 #include "temp_hum.h"
 #include "../state_machine/i2c_interface.h"
 #include "../state_machine/control.h"
+#include <cstdint>
+#include <stdint.h>
 
-Thread temp_hum_thread(osPriorityNormal,256);
+Thread temp_hum_thread(osPriorityNormal,256,nullptr,"TempThread");
 
 static ctrl_msg ctrl_msg_t;
+
+static char cmd;
+static char buff[2];
+
+/**
+* Reading function with conversion
+*/
+static void converted_reading(){
+    cmd = TH_SENSOR_MEASUREH_MASTER;
+    i2c_bus.write(TH_SENSOR_SLAVE_ADDRESS<<1,&cmd,1);
+    //Humidity
+    i2c_bus.read(TH_SENSOR_SLAVE_ADDRESS<<1,buff,2);
+    //conversion
+    ctrl_msg_t.temp_hum_msg.hum = ((125.0*(buff[0]<<8 | buff[1]))/65536.0)-6;
+
+
+    //Obtain last temperature measurement
+    cmd = TH_SENSOR_MEASURET_PREVIOUS;
+    i2c_bus.write(TH_SENSOR_SLAVE_ADDRESS<<1,&cmd,1);
+    i2c_bus.read(TH_SENSOR_SLAVE_ADDRESS<<1,buff,2);
+    //Conversion
+    ctrl_msg_t.temp_hum_msg.temp = (175.72*(buff[0]<<8 | buff[1])/65536.0)-46.85;
+}
 
 /**
 * Static function for the thread loop
@@ -16,7 +41,7 @@ static void read_temp_hum_sensor_data(){
     ctrl_msg_t.temp_hum_msg.hum = 0;
     while (true) {
         ThisThread::flags_wait_all(TEMP_HUM_SIGNAL);
-        //TODO Obtain measurements
+        converted_reading();
         ctrl_in_queue.try_put_for(Kernel::wait_for_u32_forever, &ctrl_msg_t);
     }
 }
