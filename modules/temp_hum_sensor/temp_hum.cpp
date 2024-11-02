5bee2781af5ab985c1ff2d10029a25b9ff70eda4
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-02 18:11:19 +0100

    Fixed definition on the .h, previous commit was on the cpp file

--- modules/temp_hum_sensor/temp_hum.cpp
+++ modules/temp_hum_sensor/temp_hum.cpp
@@ -1,24 +1,2 @@
 #include "mbed.h"
 #include "temp_hum.h"
-#include "mbed.h"
-/**
-* @file temp_hum.h
-*
-* @brief Temperature and Humidity sensor control module.
-*
-* @author Diego Aceituno Seoane
-*
-* Controls a Si7021-A20 temperature and Humidity sensor
-*
-*/
-
-
-/**
-* @brief Measurement thread for the temperature and humidity sensor.
-*/
-extern Thread temp_hum_thread;
-
-/**
-* @brief Inicialization of the temperature and humidity thread.
-*/
-extern void temp_hum_sensor_init();
--- modules/temp_hum_sensor/temp_hum.h
+++ modules/temp_hum_sensor/temp_hum.h
@@ -1,6 +1,25 @@
 #ifndef TEMP_HUM_H
 #define TEMP_HUM_H
+#include "mbed.h"
+/**
+* @file temp_hum.h
+*
+* @brief Temperature and Humidity sensor control module.
+*
+* @author Diego Aceituno Seoane
+*
+* Controls a Si7021-A20 temperature and Humidity sensor
+*
+*/
 
 
+/**
+* @brief Measurement thread for the temperature and humidity sensor.
+*/
+extern Thread temp_hum_thread;
 
+/**
+* @brief Inicialization of the temperature and humidity thread.
+*/
+extern void temp_hum_sensor_init();
 #endif
