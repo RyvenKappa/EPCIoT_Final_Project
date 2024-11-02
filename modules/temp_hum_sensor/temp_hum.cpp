cd2b789dbf9d2c0bdb6fbe8c04c8357bcc4152ff
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-02 18:17:53 +0100

    Empty implementation of the temperature and humidity module

--- modules/temp_hum_sensor/temp_hum.cpp
+++ modules/temp_hum_sensor/temp_hum.cpp
@@ -1,2 +1,26 @@
 #include "mbed.h"
 #include "temp_hum.h"
+#include "../state_machine/i2c_interface.h"
+#include "../state_machine/control.h"
+
+Thread temp_hum_thread(osPriorityNormal,256);
+
+static ctrl_msg ctrl_msg_t;
+
+/**
+* Static function for the thread loop
+*/
+static void read_temp_hum_sensor_data(){
+    ctrl_msg_t.type=TEMP_HUM;
+    ctrl_msg_t.temp_hum_msg.temp = 0;
+    ctrl_msg_t.temp_hum_msg.hum = 0;
+    while (true) {
+        ThisThread::flags_wait_all(TEMP_HUM_SIGNAL);
+        //TODO Obtain measurements
+        ctrl_in_queue.try_put_for(Kernel::wait_for_u32_forever, &ctrl_msg_t);
+    }
+}
+
+void temp_hum_sensor_init(){
+    temp_hum_thread.start(&read_temp_hum_sensor_data);
+}
