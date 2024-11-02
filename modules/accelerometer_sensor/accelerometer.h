e0a904afce5158938079153b81a339026252c9df
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-02 20:27:59 +0100

    Created .h file for the accelerometer with independent thread and init function

--- modules/accelerometer_sensor/accelerometer.h
+++ modules/accelerometer_sensor/accelerometer.h
@@ -1,4 +1,24 @@
 #ifndef ACCELEROMETER_H
 #define ACCELEROMETER_H
+#include "mbed.h"
+/**
+* @file accelerometer.h
+*
+* @brief Accelerometer control module.
+*
+* @author Diego Aceituno Seoane
+*
+* Controls a MMA8451Q 3-Axis sensor
+*
+*/
 
+/**
+* @brief Measurement thread for the accelerometer sensor.
+*/
+extern Thread accelerometer_thread;
+
+/**
+* @brief Inicialization of the accelerometer thread
+*/
+extern void accelerometer_sensor_init();
 #endif
