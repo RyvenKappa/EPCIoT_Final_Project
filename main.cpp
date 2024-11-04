60bd681de55999df1ab647c16a262e9a3f3832a0
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-04 18:12:33 +0100

    Include of the accelerometer in the main.cpp done

--- main.cpp
+++ main.cpp
@@ -15,8 +15,9 @@ int main()
     state_machine_init();
     moisture_sensor_init();
     brightness_sensor_init();
-    temp_hum_sensor_init();
-    accelerometer_sensor_init();
+    //color_sensor_init();
+    //temp_hum_sensor_init();
+    //accelerometer_sensor_init();
     while (true) {
         state_machine_cycle();
     }
