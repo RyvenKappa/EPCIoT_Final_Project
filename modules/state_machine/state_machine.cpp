d80b80a70610284e3b831e42c9b63796d9b7170d
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-05 00:37:09 +0100

    Initial working version of the project without GPS
    This commit represents the first working version for the initial
    validation taking place the 8th of november:
    - Redefined state machine for the requirements of the project
    - Timeout usage to launch all the flags 500ms previous to the time limit
      of the actual state.
    - Empty advanced mode
    - Auxiliar read_sensor_data() function to obtain and process all the
      messages and lastly, print onto the serial the information.
    - I2C with 400kHz frequency
    - Temporal objects for all the necesary values of the outputdata
    - Ticker and timeout attach/detach for each transition

--- modules/state_machine/state_machine.cpp
+++ modules/state_machine/state_machine.cpp
@@ -8,68 +8,163 @@
 #include "../accelerometer_sensor/accelerometer.h"
 #include "control.h"
 #include "i2c_interface.h"
+#include <cstdint>
+#include <cstdio>
 
 Queue<ctrl_msg, QUEUE_SIZE> ctrl_in_queue;
-
-enum STATES{
-    TEST,
-    NORMAL,
-    ADVANCED
-};
-
-static STATES actual_state;
 
 static BusOut board_leds(LED1,LED2,LED3);
 I2C i2c_bus(I2C_SDA_PIN,I2C_SCL_PIN);
 
 static ctrl_msg* ctrl_msg_t;
 
+static float moisture = 0.0;
+static float light = 0.0;
+static int8_t sats = 0;
+static float lat = 0.0;
+static int8_t lat_n = 0;
+static float lng = 0.0;
+static int8_t lng_w = 0;
+static int16_t altitude = 0;
+static char alt_units = 'm';
+static int8_t time_h = 0;
+static int8_t time_m = 0;
+static int8_t time_s = 0;
+static int16_t color_clear = 0;
+static int16_t color_red = 0;
+static int16_t color_green = 0;
+static int16_t color_blue = 0;
+static float x_acc = 0.0;
+static float y_acc = 0.0;
+static float z_acc = 0.0;
+static float temp = 0.0;
+static float humidity = 0.0;
 
-static Ticker ticker;
+
+
+static Ticker ticker;   //TODO change to lp
+static Timeout timeout; //TODO change to lp
+static volatile bool timeout_event = false;
 static volatile bool ticker_event = false;
-
 /**
 * Ticker ISR
 */
-void ticker_handler(){
+void ticker_isr(){
     ticker_event = true;
 }
+void timeout_isr(){
+    timeout_event = true;
+}
+
+
+//****STATE MACHINE*******************
+enum STATES{
+    TEST,
+    NORMAL,
+    ADVANCED
+};
+static STATES actual_state;
 
 void state_machine_init(){
     actual_state = TEST;
     board_leds.write(1);
-    ticker.attach(ticker_handler,5s);
+    ticker.attach(ticker_isr,2s);
+    timeout.attach(timeout_isr, 1800ms);
+    i2c_bus.frequency(400000);
+}
+/**
+* Auxiliar function to read data
+*/
+static void read_sensors_data(){
+    while (ctrl_in_queue.try_get(&ctrl_msg_t)) {
+        switch (ctrl_msg_t->type) {
+            case ACCELEROMETER:
+                x_acc = ctrl_msg_t->accelerometer_msg.x_acc;
+                y_acc = ctrl_msg_t->accelerometer_msg.y_acc;
+                z_acc = ctrl_msg_t->accelerometer_msg.z_acc;
+                break;
+            case GPS:
+                break;
+            case BRIGHTNESS:
+                light = ctrl_msg_t->brightness_msg;
+                break;
+            case TEMP_HUM:
+                temp = ctrl_msg_t->temp_hum_msg.temp;
+                humidity = ctrl_msg_t->temp_hum_msg.hum;
+                break;
+            case COLOR:
+                color_clear = ctrl_msg_t->color_msg.clear;
+                color_red = ctrl_msg_t->color_msg.red;
+                color_green = ctrl_msg_t->color_msg.green;
+                color_blue = ctrl_msg_t->color_msg.blue;
+                //TODO maximo
+                break;
+            case MOISTURE:
+                moisture = ctrl_msg_t->moisture_msg;
+                break;
+        }
+    }
+    printf("MODE: %d\n",actual_state);
+    printf("SOIL MOISTURE: %.1f%%\n",moisture);
+    printf("LIGHT: %.1f\n",light);
+    printf("GPS: \n");
+    printf("COLOR SENSOR: Clear: %d Red: %d Green: %d Blue: %d -- Dominant color: ??\n",color_clear,color_red,color_green,color_blue);
+    printf("ACCELEROMETERS: X_axis: %.1f m/s², Y_axis: %.1f m/s², Z_axis: %.1f m/s²\n",x_acc,y_acc,z_acc);
+    printf("TEMP/HUM: Temperature:\t%.1f °C,\t\tRelative Humidity: %.1f%%\n\n\n",temp,humidity);
 }
 
 void state_machine_cycle(){
     switch (actual_state) {
         case TEST:
-            if(ticker_event){
-                ticker_event=false;
+            if(timeout_event){
+                timeout_event = false;
                 moisture_thread.flags_set(MOISTURE_SIGNAL);
                 brightness_thread.flags_set(BRIGHTNESS_SIGNAL);
-                //accelerometer_thread.flags_set(ACCELEROMETER_SIGNAL);
+                accelerometer_thread.flags_set(ACCELEROMETER_SIGNAL);
+                brightness_thread.flags_set(BRIGHTNESS_SIGNAL);
+                temp_hum_thread.flags_set(TEMP_HUM_SIGNAL);
+                color_thread.flags_set(COLOR_SIGNAL);
             }
-            if(ctrl_in_queue.try_get(&ctrl_msg_t)){
-                if (ctrl_msg_t->type==MOISTURE){
-                    printf("Value of the moisture: %f\n",ctrl_msg_t->moisture_msg);
-                }else if (ctrl_msg_t->type==BRIGHTNESS) {
-                    printf("Value of the brightness: %f\n",ctrl_msg_t->brightness_msg);
-                }
-            }
+            if(ticker_event){
+                ticker_event=false;
+                read_sensors_data();
+                timeout.detach();
+                timeout.attach(timeout_isr, 1800ms);
 
+            }
             if(button_pressed_msg){
                 button_pressed_msg = 0;
                 actual_state = NORMAL;
                 ticker.detach();
+                timeout.detach();
+                ticker.attach(ticker_isr,30s);
+                timeout.attach(timeout_isr, 29800ms);
                 board_leds.write(2);
                 printf("Test a Normal\n");
             }
         break;
         case NORMAL:
+            if(timeout_event){
+                timeout_event = false;
+                moisture_thread.flags_set(MOISTURE_SIGNAL);
+                brightness_thread.flags_set(BRIGHTNESS_SIGNAL);
+                accelerometer_thread.flags_set(ACCELEROMETER_SIGNAL);
+                brightness_thread.flags_set(BRIGHTNESS_SIGNAL);
+                temp_hum_thread.flags_set(TEMP_HUM_SIGNAL);
+                color_thread.flags_set(COLOR_SIGNAL);
+            }
+            if(ticker_event){
+                ticker_event=false;
+                read_sensors_data();
+                timeout.detach();
+                timeout.attach(timeout_isr, 29800ms);
+
+            }
             if (button_pressed_msg) {
                 button_pressed_msg = 0;
                 actual_state = ADVANCED;
+                ticker.detach();
+                timeout.detach();
                 board_leds.write(4);
                 printf("Normal a Advanced\n");
             }
@@ -78,6 +173,8 @@ void state_machine_cycle(){
             if (button_pressed_msg) {
                 button_pressed_msg = 0;
                 actual_state = TEST;
+                ticker.attach(ticker_isr,2s);
+                timeout.attach(timeout_isr, 1800ms);
                 board_leds.write(1);
                 printf("Advanced a Test\n");
             }
