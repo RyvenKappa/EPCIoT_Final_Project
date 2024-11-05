c886782256530ac04ab90c5ac9fe3d24708c5720
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-05 15:18:00 +0100

    Implementation of the brightness reading as defined in the  .h

--- modules/brightness_sensor/brightness_sensor.cpp
+++ modules/brightness_sensor/brightness_sensor.cpp
@@ -4,26 +4,12 @@
 
 #define BRIGHTNESS_READ_PIN PA_4
 
-Thread brightness_thread(osPriorityNormal,256,nullptr,"BrightnessThread");
-
 static AnalogIn value_reader(BRIGHTNESS_READ_PIN);
 
-static ctrl_msg ctrl_msg_t;
 
 /**
 * Static function for the thread loop
 */
-static void read_brightness_sensor_data(){
-    ctrl_msg_t.type=BRIGHTNESS;
-    ctrl_msg_t.brightness_msg = 0;
-    while (true){
-        ThisThread::flags_wait_all(BRIGHTNESS_SIGNAL);
-        ctrl_msg_t.brightness_msg = ((float)value_reader.read_u16()*100)/0xFFFF;
-        ctrl_in_queue.try_put_for(Kernel::wait_for_u32_forever, &ctrl_msg_t);
-    }
-}
-
-
-void brightness_sensor_init(){
-    brightness_thread.start(&read_brightness_sensor_data);
+float read_brightness_sensor_data(){
+    return ((float)value_reader.read_u16()*100)/0xFFFF;
 }
