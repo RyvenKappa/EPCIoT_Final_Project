9a224eb13d4d57353826a1581278af70dc488b81
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-11 20:22:37 +0100

    Moisture sensor powering on and off

--- modules/moisture_sensor/moisture_sensor.cpp
+++ modules/moisture_sensor/moisture_sensor.cpp
@@ -1,14 +1,19 @@
 #include "moisture_sensor.h"
 #include "../state_machine/control.h"
 
-#define MOISTURE_READ_PIN PA_5
-//TODO Por ahora no le voy a meter la funcionalidad del drive por GPIO #define MOISTURE_POWER_PIN 
+#define MOISTURE_READ_PIN PA_0
+#define MOISTURE_POWER_PIN PA_13
 
 static AnalogIn value_reader(MOISTURE_READ_PIN);
+static DigitalOut power_pin(MOISTURE_POWER_PIN);
 
 /**
 * Static function for the thread loop
 */
 float read_moisture_sensor_data(){
-    return ((float)value_reader.read_u16()*100)/0xFFFF;
+    power_pin.write(1);
+    ThisThread::sleep_for(1ms);
+    float return_value = ((float)value_reader.read_u16()*100)/0xFFFF;
+    power_pin.write(0);
+    return return_value;
 }
