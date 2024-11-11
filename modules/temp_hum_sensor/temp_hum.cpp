670558ef2a7451feee37ee08a978de0df9f29482
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-11 18:53:21 +0100

    Removed temp_hum thread to make only a reading call

--- modules/temp_hum_sensor/temp_hum.cpp
+++ modules/temp_hum_sensor/temp_hum.cpp
@@ -1,11 +1,9 @@
 #include "mbed.h"
 #include "temp_hum.h"
-#include "../state_machine/i2c_interface.h"
+#include "../i2c/i2c_interface.h"
 #include "../state_machine/control.h"
 #include <cstdint>
 #include <stdint.h>
-
-Thread temp_hum_thread(osPriorityNormal,256,nullptr,"TempThread");
 
 static ctrl_msg ctrl_msg_t;
 
@@ -17,7 +15,6 @@ static char buff[2];
 */
 static void converted_reading(){
     cmd = TH_SENSOR_MEASUREH_MASTER;
-    i2c_bus.lock();
     i2c_bus.write(TH_SENSOR_SLAVE_ADDRESS<<1,&cmd,1,true);
     //Humidity
     i2c_bus.read(TH_SENSOR_SLAVE_ADDRESS<<1,buff,2);
@@ -29,7 +26,6 @@ static void converted_reading(){
     cmd = TH_SENSOR_MEASURET_PREVIOUS;
     i2c_bus.write(TH_SENSOR_SLAVE_ADDRESS<<1,&cmd,1);
     i2c_bus.read(TH_SENSOR_SLAVE_ADDRESS<<1,buff,2);
-    i2c_bus.unlock();
     //Conversion
     ctrl_msg_t.temp_hum_msg.temp = (175.72*(buff[0]<<8 | buff[1])/65536.0)-46.85;
 }
@@ -37,17 +33,10 @@ static void converted_reading(){
 /**
 * Static function for the thread loop
 */
-static void read_temp_hum_sensor_data(){
+void temp_hum_sensor_read(){
     ctrl_msg_t.type=TEMP_HUM;
     ctrl_msg_t.temp_hum_msg.temp = 0;
     ctrl_msg_t.temp_hum_msg.hum = 0;
-    while (true) {
-        ThisThread::flags_wait_all(TEMP_HUM_SIGNAL);
-        converted_reading();
-        ctrl_in_queue.try_put_for(Kernel::wait_for_u32_forever, &ctrl_msg_t);
-    }
-}
-
-void temp_hum_sensor_init(){
-    temp_hum_thread.start(&read_temp_hum_sensor_data);
+    converted_reading();
+    ctrl_in_queue.try_put_for(Kernel::wait_for_u32_forever, &ctrl_msg_t);
 }
