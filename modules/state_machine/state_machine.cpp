a3968f273081f439155839e207498321b644a985
Diego Aceituno Seoane <diego.aceituno@alumnos.upm.es>
2024-11-11 23:56:50 +0100

    Added NM3, NM4 & NM5 functionalities

--- modules/state_machine/state_machine.cpp
+++ modules/state_machine/state_machine.cpp
@@ -46,6 +46,40 @@ static float z_acc = 0.0;
 static float temp = 0.0;
 static float humidity = 0.0;
 
+static uint8_t hour_limit_counter = 0; //This counter goes up by 1 every 30 seconds on NORMAL and ADVANCED mode.
+
+static bool started = false;
+
+//Mean, Maximum and Minimum values
+static float min_temp = 0.0;
+static float max_temp = 0.0;
+static float temp_adder = 0.0;
+
+static float min_humidity = 0.0;
+static float max_humidity = 0.0;
+static float humidity_adder = 0.0;
+
+static float min_light = 0.0;
+static float max_light = 0.0;
+static float light_adder = 0.0;
+
+static float min_moisture = 0.0;
+static float max_moisture = 0.0;
+static float moisture_adder = 0.0;
+
+//Minimum and maximum values only for acceleration
+static float min_x = 0.0;
+static float max_x = 0.0;
+static float min_y = 0.0;
+static float max_y = 0.0;
+static float min_z = 0.0;
+static float max_z = 0.0;
+
+//Dominant color on the plant
+static uint8_t red_ocurrences = 0;
+static uint8_t green_ocurrences = 0;
+static uint8_t blue_ocurrences = 0;
+
 static LowPowerTicker ticker;
 static LowPowerTimeout timeout;
 static volatile bool timeout_event = false;
@@ -79,6 +113,54 @@ void state_machine_init(){
     timeout.attach(timeout_isr, 1700ms);
     i2c_bus.frequency(400000);
 }
+
+/**
+* Auxiliar function to print the mean, min and max values of the variables to the serial.
+*/
+static void hour_data_to_serial(){
+    printf("1 Hour has passed, this is the data analysis:\n");
+    printf("\tTemperature:   min:%.1f°C, max:%.1f°C, mean:%.1f°C.\n",min_temp,max_temp,(temp_adder/hour_limit_counter));
+    printf("\tHumidity:   min:%.1f%%, max:%.1f%%, mean:%.1f%%.\n",min_humidity,max_humidity,(humidity_adder/hour_limit_counter));
+    printf("\tLight:   min:%.1f%%, max:%.1f%%, mean:%.1f%%.\n",min_light,max_light,(light_adder/hour_limit_counter));
+    printf("\tMoisture:   min:%.1f%% max:%.1f%%, mean:%.1f%%.\n",min_moisture,max_moisture,(moisture_adder/hour_limit_counter));
+    printf("\tAccelerometer_X:   min:%.1fm/s², max:%.1fm/s².\n",min_x,max_x);
+    printf("\tAccelerometer_Y:   min:%.1fm/s², max:%.1fm/s².\n",min_y,max_y);
+    printf("\tAccelerometer_Z:   min:%.1fm/s², max:%.1fm/s².\n",min_z,max_z);
+    //Most dominant color
+    if ((red_ocurrences>green_ocurrences) && (red_ocurrences>blue_ocurrences)){
+        printf("\tMost dominant color: red\n");
+    }else if ((green_ocurrences>red_ocurrences) && (green_ocurrences>blue_ocurrences)) {
+        printf("\tMost dominant color: green\n");
+    }else if ((blue_ocurrences>red_ocurrences) && (blue_ocurrences>green_ocurrences)) {
+        printf("\tMost dominant color: blue\n");
+    }else{
+        printf("\tMost dominant color: none\n");
+    }
+    hour_limit_counter=0;
+    min_temp = 0;
+    max_temp = 0;
+    temp_adder = 0;
+    min_humidity = 0;
+    max_humidity = 0;
+    humidity_adder = 0;
+    min_light = 0;
+    max_light = 0;
+    light_adder = 0;
+    min_moisture = 0;
+    max_moisture = 0;
+    moisture_adder = 0;
+    min_x = 0;
+    max_x = 0;
+    min_y = 0;
+    max_y = 0;
+    min_z = 0;
+    max_z = 0;
+    red_ocurrences = 0;
+    green_ocurrences = 0;
+    blue_ocurrences = 0;
+    started = false;
+}
+
 /**
 * Auxiliar function to print the standard message to the serial when no emergency.
 */
@@ -87,25 +169,31 @@ static void data_to_serial(){
     if (actual_state!=TEST){
         //Temp limits
         if ((temp<10.0) || (temp>35.0)){
-            printf("TEMPERATURE %.1f%% EXCEDING LIMITS, lower or raise the air temperature of the room!\n",temp);
+            printf("TEMPERATURE %.1f°C EXCEDING LIMITS, lower or raise the air temperature of the room!\n",temp);
+            //TODO LED
         }
         //Humidity limits
         if((humidity<25) || (humidity>75.0)){
-            printf("HUMIDITY %.1f%% EXCEDING LIMITS, increase or reduce the air flow of the room!\n",temp);
+            printf("HUMIDITY %.1f%% EXCEDING LIMITS, increase or reduce the air flow of the room!\n",humidity);
+            //TODO LED
         }
         //Light limits, check at daytime, do the inverse for nighttime
         if(light<25){
-            printf("LIGHT %.1f%% TOO LOW!\n",temp);
+            printf("LIGHT %.1f%% TOO LOW!\n",light);
+            //TODO LED
         }
         if (light>75){
-            printf("LIGHT %.1f%% TOO BRIGHT!\n",temp);
+            printf("LIGHT %.1f%% TOO BRIGHT!\n",light);
+            //TODO LED
         }
         //Moisture limits TODO check with a plant
         if(moisture<15){
-            printf("MOISTURE %.1f%% TOO LOW!\n",temp);
+            printf("MOISTURE %.1f%% TOO LOW!\n",moisture);
+            //TODO LED
         }
         if (moisture>60){
-            printf("MOISTURE %.1f%% TOO BRIGHT!\n",temp);
+            printf("MOISTURE %.1f%% TOO BRIGHT!\n",moisture);
+            //TODO LED
         }
         //Color TODO LIMITS OF THE COLOR
 
@@ -117,20 +205,27 @@ static void data_to_serial(){
     printf("\tLIGHT: %.1f%%\n",light);
     printf("\tGPS: #Sats: %d Lat(UTC): %f %c Long(UTC): %f %c Altitude: %d %c GPS time: %d:%d:%d CET\tDay %d, Month %d, Year %d\n",sats,lat,lat_n,lng,lng_w,altitude,altitude_c,time_h,time_m,time_s,time_day,time_month,time_year);
     printf("\tCOLOR SENSOR: Clear: %d Red: %d Green: %d Blue: %d -- Dominant color: ",color_clear,color_red,color_green,color_blue);
+    //Calculate dominant color
     if ((color_red>color_blue) && (color_red>color_green)){
         printf("red\n");
         if (actual_state==TEST){
             change_led_color(true, false, false);
+        }else{
+            red_ocurrences += 1;
         }
     }else if ((color_blue>color_red) && (color_blue>color_green)) {
         printf("blue\n");
         if (actual_state==TEST){
             change_led_color(false, false, true);
+        }else{
+            blue_ocurrences += 1;
         }
     }else if ((color_green>color_red) && (color_green>color_blue)) {
         printf("green\n");
         if (actual_state==TEST){
             change_led_color(false, true, false);
+        }else{
+            green_ocurrences += 1;
         }
     }else {
         printf("none\n");
@@ -138,6 +233,7 @@ static void data_to_serial(){
             change_led_color(false, false, false);
         }
     }
+
     printf("\tACCELEROMETERS: X_axis: %.1f m/s², Y_axis: %.1f m/s², Z_axis: %.1f m/s²\n",x_acc,y_acc,z_acc);
     printf("\tTEMP/HUM: Temperature:\t%.1f °C,\tRelative Humidity: %.1f%%\n\n\n",temp,humidity);
 }
@@ -181,6 +277,44 @@ static void read_sensors_data(){
     }
     moisture = read_moisture_sensor_data();
     light = read_brightness_sensor_data();
+    if (!started){
+        //Assign min and max
+        min_x = x_acc;
+        max_x = x_acc;
+        min_y = y_acc;
+        max_y = y_acc;
+        min_z = z_acc;
+        max_z = z_acc;
+        min_temp = temp;
+        max_temp = temp;
+        min_humidity = humidity;
+        max_humidity = humidity;
+        min_moisture = moisture;
+        max_moisture = moisture;
+        min_light = light;
+        max_light = light;
+        started = true;
+    }
+
+    //Max and min values checking
+    if (actual_state != TEST){
+        //Acceleration, min and max value over hour
+        if(x_acc>max_x){max_x = x_acc;}else if (x_acc<min_x) {min_x = x_acc;}
+        if(y_acc>max_y){max_y = y_acc;}else if (y_acc<min_y) {min_y = y_acc;}
+        if(z_acc>max_z){max_z = z_acc;}else if (z_acc<min_z) {min_z = z_acc;}
+        //Temperature
+        if(temp>max_temp){max_temp = temp;}else if (temp<min_temp) {min_temp = temp;}
+        temp_adder += temp;
+        //Humidity
+        if(humidity>max_humidity){max_humidity = humidity;}else if (humidity<min_humidity) {min_humidity = humidity;}
+        humidity_adder += humidity;
+        //Humidity
+        if(light>max_light){max_light = light;}else if (light<min_light) {min_light = light;}
+        light_adder += light;
+        //Moisture
+        if(moisture>max_moisture){max_moisture = moisture;}else if (moisture<min_moisture) {min_moisture = moisture;}
+        moisture_adder += moisture;
+    }
 }
 
 void state_machine_cycle(){
@@ -197,7 +331,6 @@ void state_machine_cycle(){
                 data_to_serial();
                 ticker.attach(ticker_isr,2000ms);
                 timeout.attach(timeout_isr, 1700ms);
-
             }
             if(button_pressed_msg){
                 button_pressed_msg = 0;
@@ -223,7 +356,11 @@ void state_machine_cycle(){
             if(ticker_event){
                 ticker_event=false;
                 read_sensors_data();
+                hour_limit_counter += 1;
                 data_to_serial();
+                if (hour_limit_counter==120) {
+                    hour_data_to_serial();
+                }
                 ticker.attach(ticker_isr,30000ms);
                 timeout.attach(timeout_isr, 29700ms);
 
@@ -250,6 +387,7 @@ void state_machine_cycle(){
                 while(!ctrl_in_queue.empty()){
                     ctrl_in_queue.try_get(&ctrl_msg_t); //Empty possible previous messages
                 }
+                //TODO RESET HOUR_COUNTER AND ALL THE MIN_MAX_MEAN VALUES
                 printf("Advanced a Test\n");
             }
         break;
