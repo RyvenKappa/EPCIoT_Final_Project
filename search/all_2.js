2d357cff7c3e4497cde0b2cbff3db7da515a88db
RyvenKappa <RyvenKappa@users.noreply.github.com>
2024-11-18 00:46:33 +0000

    deploy: 633b083eb47b8b10c88324732d038d2b9134027a

--- search/all_2.js
+++ search/all_2.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['change_5fled_5fcolor_0',['change_led_color',['../led__module_8h.html#a7e4296c322d27ba8c0982e1ca95ba70b',1,'led_module.cpp']]],
-  ['color_5fmsg_5ft_1',['color_msg_t',['../structcolor__msg__t.html',1,'']]],
-  ['color_5fsensor_2eh_2',['color_sensor.h',['../color__sensor_8h.html',1,'']]],
-  ['color_5fsensor_5finit_3',['color_sensor_init',['../color__sensor_8h.html#a975af4d47a2cebfeca98ce33012fa92f',1,'color_sensor.cpp']]],
-  ['color_5fsensor_5fread_4',['color_sensor_read',['../color__sensor_8h.html#a768a4293cce7bc6043c01bd73a5b97f9',1,'color_sensor.cpp']]],
-  ['communications_20for_20iot_5',['Final Project repository for the Embedded Platforms and Communications for IoT',['../md_README.html',1,'']]],
-  ['control_2eh_6',['control.h',['../control_8h.html',1,'']]],
-  ['ctrl_5fmsg_7',['ctrl_msg',['../structctrl__msg.html',1,'']]]
+  ['clean_5finterrupt_1',['clean_interrupt',['../color__sensor_8h.html#ad2ed44d5582579e95a2b5add55045c2c',1,'color_sensor.h']]],
+  ['color_5fmsg_5ft_2',['color_msg_t',['../structcolor__msg__t.html',1,'']]],
+  ['color_5fsensor_2eh_3',['color_sensor.h',['../color__sensor_8h.html',1,'']]],
+  ['color_5fsensor_5finit_4',['color_sensor_init',['../color__sensor_8h.html#a975af4d47a2cebfeca98ce33012fa92f',1,'color_sensor.cpp']]],
+  ['color_5fsensor_5fread_5',['color_sensor_read',['../color__sensor_8h.html#a768a4293cce7bc6043c01bd73a5b97f9',1,'color_sensor.cpp']]],
+  ['communications_20for_20iot_6',['Final Project repository for the Embedded Platforms and Communications for IoT',['../md_README.html',1,'']]],
+  ['control_2eh_7',['control.h',['../control_8h.html',1,'']]],
+  ['ctrl_5fmsg_8',['ctrl_msg',['../structctrl__msg.html',1,'']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['change_5fled_5fcolor_0',['change_led_color',['../led__module_8h.html#a7e4296c322d27ba8c0982e1ca95ba70b',1,'led_module.cpp']]],
-  ['color_5fsensor_5finit_1',['color_sensor_init',['../color__sensor_8h.html#a975af4d47a2cebfeca98ce33012fa92f',1,'color_sensor.cpp']]],
-  ['color_5fsensor_5fread_2',['color_sensor_read',['../color__sensor_8h.html#a768a4293cce7bc6043c01bd73a5b97f9',1,'color_sensor.cpp']]]
+  ['clean_5finterrupt_1',['clean_interrupt',['../color__sensor_8h.html#ad2ed44d5582579e95a2b5add55045c2c',1,'color_sensor.h']]],
+  ['color_5fsensor_5finit_2',['color_sensor_init',['../color__sensor_8h.html#a975af4d47a2cebfeca98ce33012fa92f',1,'color_sensor.cpp']]],
+  ['color_5fsensor_5fread_3',['color_sensor_read',['../color__sensor_8h.html#a768a4293cce7bc6043c01bd73a5b97f9',1,'color_sensor.cpp']]]
 ];
